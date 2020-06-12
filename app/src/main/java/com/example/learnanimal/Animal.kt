9978bf7ba72943c42cb03510a5893abc2f246b87
Muhittinkaya <mhttnkaya@gmail.com>
2020-06-12 17:53:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/learnanimal/Animal.kt
@@ -0,0 +1,12 @@
+package com.example.learnanimal
+
+class Animal {
+    var name:String?=null
+    var description:String?=null
+    var image:Int?=null
+    constructor(name:String, description:String, image:Int){
+        this.name = name
+        this.description = description
+        this.image = image
+    }
+}
---
+++ app/src/main/java/com/example/learnanimal/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.learnanimal
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.animal_ticket.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    var listOfAnimals = ArrayList<Animal>()
+    var adapter: AnimalsAdapter?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // add the animals to array list
+        listOfAnimals.add(Animal("Bear","Bears are dangerous animals. They eat fishes mostly",R.drawable.bear))
+        listOfAnimals.add(Animal("Bird","Birds can fly. Their sound is very relaxing and beautiful. There are so many variants of birds.",R.drawable.bird))
+        listOfAnimals.add(Animal("Camel","Mostly seen in the Arab peninsula. They have hump.",R.drawable.camel))
+        listOfAnimals.add(Animal("Cat","Cats are very cute. But also they don't care anything. They live how they want :D",R.drawable.cat))
+        listOfAnimals.add(Animal("Chicken","Chickens have wing but they can't fly. They spawn eggs.",R.drawable.chicken))
+        listOfAnimals.add(Animal("Dog","Dogs are very friendly to humans. They protect you outer dangers.",R.drawable.dog))
+        listOfAnimals.add(Animal("Lion","Lions are the king of the jungle. They eat meat and they are the symbol of power.",R.drawable.lion))
+        listOfAnimals.add(Animal("Rabbit","Rabbit eats carrot and it runs very fast! Most favorite rabbit is Bugs Bunny :)",R.drawable.rabbit))
+        listOfAnimals.add(Animal("Sheep","Sheep is very cute. You can drink its milk. And they are mostly curly.",R.drawable.sheep))
+        listOfAnimals.add(Animal("Snake","Snake is a reptile. Some snakes are poisonous. Be careful!",R.drawable.snake))
+
+        adapter = AnimalsAdapter(this,listOfAnimals)
+        listview_main.adapter = adapter
+    }
+
+    class AnimalsAdapter: BaseAdapter{
+
+        var listOfAnimals = ArrayList<Animal>()
+        var context:Context?=null
+        constructor(context: Context, listOfAnimals: ArrayList<Animal>):super(){
+            this.listOfAnimals = listOfAnimals
+            this.context = context
+        }
+
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val animal = listOfAnimals[position]
+            var inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            var myView = inflater.inflate(R.layout.animal_ticket,null)
+            myView.textViewName.text = animal.name!!
+            myView.textViewDescription.text = animal.description!!
+            myView.image_animal.setImageResource(animal.image!!)
+            return myView
+
+        }
+
+        override fun getItem(position: Int): Any {
+            return position
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return listOfAnimals.size
+        }
+
+    }
+}
